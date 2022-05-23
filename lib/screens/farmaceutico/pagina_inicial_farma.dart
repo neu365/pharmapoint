e669d0245caf6c23de30bb583121926ff18bec1d
Neuza Claro <neuzaclaro@gmail.com>
2022-05-23 12:13:17 +0100

    butão para editar perfil

--- lib/screens/farmaceutico/pagina_inicial_farma.dart
+++ lib/screens/farmaceutico/pagina_inicial_farma.dart
@@ -3,13 +3,47 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:pharmapoint/widgets/widgets2/farma/chat_pharma/chat_button.dart';
 import 'package:pharmapoint/widgets/widgets2/farma/chat_pharma/recentes_butao.dart';
-import 'package:pharmapoint/widgets/widgets2/user/novopedido_butao.dart';
 
 class PaginaInicialFarma extends StatelessWidget {
   const PaginaInicialFarma({Key key}) : super(key: key);
+  List<DropdownMenuItem<String>> get dropdownItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(
+          value: 'logout',
+          child: Row(
+            children: const [
+              Icon(
+                Icons.exit_to_app,
+                color: Colors.green,
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text('Logout'),
+            ],
+          )),
+      DropdownMenuItem(
+          value: 'edit',
+          child: Row(
+            children: const [
+              Icon(
+                Icons.edit,
+                color: Colors.green,
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text('Editar perfil'),
+            ],
+          )),
+      DropdownMenuItem(value: ' ', child: Container()),
+    ];
+    return menuItems;
+  }
 
   @override
   Widget build(BuildContext context) {
+    String selectedValue = " ";
     return Scaffold(
       appBar: AppBar(
         title: const Text('PharmaPoint - Farmacêutico'),
@@ -17,42 +51,23 @@ class PaginaInicialFarma extends StatelessWidget {
         centerTitle: true,
         actions: [
           DropdownButton(
+            value: selectedValue,
             underline: Container(),
             icon: const Icon(
               Icons.more_vert,
               color: Colors.white,
             ),
-            items: [
-              DropdownMenuItem(
-                value: 'logout',
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.exit_to_app,
-                      color: Colors.green,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text('Logout'),
-                    Icon(
-                      Icons.edit,
-                      color: Colors.green,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text('Editar perfil'),
-                  ],
-                ),
-              ),
-            ],
+            items: dropdownItems,
             onChanged: (itemIdentifier) {
+              if (itemIdentifier == ' ') {}
               if (itemIdentifier == 'logout') {
                 FirebaseAuth.instance.signOut();
               }
+              if (itemIdentifier == 'edit') {
+                //escrever aqui a classe para a página de editar perfil
+              }
             },
-          )
+          ),
         ],
       ),
       body: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
